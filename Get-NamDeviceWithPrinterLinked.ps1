fc17b48b3e351bc0064423033f79d244c4ff6276
pzohaycuoi <pzohaycuoi@gmail.com>
2021-07-28 22:55:12 +0700

    update

--- Get-NamDeviceWithPrinterLinked.ps1
+++ Get-NamDeviceWithPrinterLinked.ps1
@@ -1,10 +1,10 @@
-$importServerList = Import-Csv -Path filesystem::.\serverList.csv
+$importServerList = Import-Csv -Path filesystem::.\serverList2.csv
 $exportFileName = "serverUseWithPrinterLinked.csv"
 New-Item -Path filesystem::.\ -Name $exportFileName -ItemType "File" -Force
 
 foreach ($server in $importServerList) {
   Write-Output "Getting computer's printer information: $($server.name)"
-  $checkIfPrintServ = (Get-Printer | Where Shared -eq $true).Count
+  $checkIfPrintServ = Get-Printer -ComputerName $server.name | Where Shared -eq $true
   if (-not($checkIfPrintServ -eq $null)) {
     $server | Export-Csv -Path filesystem::.\$exportFileName -Append -Force -NoTypeInformation
     Write-Output "Server $($server.name) have printer connected."
